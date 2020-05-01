f50654f830a5a8d715c1b70a6f76b6d4244b45c2
Radu <avadanii@tiscali.it>
2020-05-01 09:52:00 +0300

    profile page

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -58,6 +58,52 @@ const Header = () => {
               </NavItem>
             </React.Fragment>
           )}
+          {isAuth() && isAuth().role === 0 && (
+            <NavItem>
+              <Link href='/user'>
+                <NavLink>
+                  <svg
+                    className='bi bi-person-fill'
+                    width='1em'
+                    height='1em'
+                    viewBox='0 0 16 16'
+                    fill='currentColor'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      d='M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 100-6 3 3 0 000 6z'
+                      clipRule='evenodd'
+                    />
+                  </svg>{' '}
+                  <span>{`${isAuth().name}'s Dashboard`}</span>
+                </NavLink>
+              </Link>
+            </NavItem>
+          )}
+          {isAuth() && isAuth().role === 1 && (
+            <NavItem>
+              <Link href='/admin'>
+                <NavLink>
+                  <svg
+                    className='bi bi-person-fill'
+                    width='1em'
+                    height='1em'
+                    viewBox='0 0 16 16'
+                    fill='currentColor'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      d='M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 100-6 3 3 0 000 6z'
+                      clipRule='evenodd'
+                    />
+                  </svg>{' '}
+                  <span>{`${isAuth().name}'s Dashboard`}</span>
+                </NavLink>
+              </Link>
+            </NavItem>
+          )}
         </Nav>
         {isAuth() && (
           <NavLink
